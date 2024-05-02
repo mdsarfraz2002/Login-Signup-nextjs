5ffcf3a0d2eb76311446c645f15f89d4545e4ced
“rahulthedev-ai” <rahulkbhandari5849@gmail.com>
2024-05-02 16:42:22 +0530

    v2

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,24 @@ export default function Home(){
       <h1> Registraion </h1>
       <form action="/api/register" method="post">
         <label>
-          EMail Address
+          Email Address
         </label>
         <input type='email' name='email' placeholder='Type your email'></input>
+
         <label>
           Pasword
         </label>
         <input type='password' name='password' placeholder='Type your password'></input>
+
+        <label>
+          Full Name
+        </label>
+        <input type='text' name='fullName' placeholder='Type your full name'></input>
+        <label>
+          Lable Studio Id
+        </label>
+        <input type='text' name='lableStudioId' placeholder='Type your Lable Studio Id'></input>
+        
         <input type='submit' value='Register'></input>
 
       </form>
@@ -18,7 +29,7 @@ export default function Home(){
       <h1>Login</h1>
       <form action="/api/login" method="post">
         <label>
-          EMail Address
+          Email Address
         </label>
         <input type='email' name='email' placeholder='Type your email'></input>
         <label>
